91995abb94cd87e10d017a21e3e1052cb6aa178b
CJ Cullen <cjcullen@google.com>
2016-03-10 11:09:12 -0800

    Cache retrieved GCP tokens

--- plugin/pkg/client/transport/gcp/gcp.go
+++ plugin/pkg/client/transport/gcp/gcp.go
@@ -17,8 +17,13 @@ limitations under the License.
 package gcp
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
+	"path"
+	"time"
 
+	"github.com/golang/glog"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -38,7 +43,66 @@ func (*gcpRoundTripperPlugin) RoundTripper(rt http.RoundTripper) (http.RoundTrip
 		return nil, err
 	}
 	return &oauth2.Transport{
-		Source: ts,
+		Source: &cachedTokenSource{ts, "~/.kube/access_token"},
 		Base:   rt,
 	}, nil
+}
+
+type cachedTokenSource struct {
+	source    oauth2.TokenSource
+	tokenFile string
+}
+
+// Token returns an OAuth2 access token, either from a file, or retrived from
+// the original token source.
+func (c *cachedTokenSource) Token() (*oauth2.Token, error) {
+	// If we have a valid, cached access token, use it.
+	if tok, err := parseTokenFromFile(c.tokenFile); err == nil && isValid(tok) {
+		return tok, nil
+	}
+	tok, err := c.source.Token()
+	if err != nil {
+		return nil, err
+	}
+	// Cache the token on disk.
+	if err := saveTokenToFile(tok, c.tokenFile); err != nil {
+		glog.Warningf("Failed to save token to file: %v", err)
+	}
+	return tok, nil
+}
+
+func isValid(tok *oauth2.Token) bool {
+	return tok.Valid() && time.Now().Before(tok.Expiry.Add(-30*time.Second))
+}
+
+func parseTokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	var t oauth2.Token
+	if err := json.NewDecoder(f).Decode(&t); err != nil {
+		return nil, err
+	}
+	return &t, nil
+}
+
+func saveTokenToFile(token *oauth2.Token, file string) error {
+	tok := *token
+	// Don't try to persist the long-term credential, even if it exists.
+	tok.RefreshToken = ""
+	dir := path.Dir(file)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+	}
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	if err := json.NewEncoder(f).Encode(&tok); err != nil {
+		return err
+	}
+	return nil
 }
